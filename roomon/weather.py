d620ee7d9747a6fdb22bf6cee68a89194989189d
Ken Lim <ken@unintendeduse.com>
2021-01-26 20:01:35 +0000

    Force a new req to OpenWeatherMap

--- roomon/weather.py
+++ roomon/weather.py
@@ -9,17 +9,17 @@ config.read("settings.ini")
 
 apiSettings = config["openweathermap"]
 
-r = requests.get("http://api.openweathermap.org/data/2.5/weather", 
-    {"id" : apiSettings["cityId"], 
-    "units" : apiSettings["units"],
-    "appid" : apiSettings["apiKey"]
-    })
 
 tempGauge = Gauge('openweathermap_temp', 'Temperature outside in C')
 
 start_http_server(8001)
-print(r.url)
+
 while True:
+    r = requests.get("http://api.openweathermap.org/data/2.5/weather", 
+        {"id" : apiSettings["cityId"], 
+        "units" : apiSettings["units"],
+        "appid" : apiSettings["apiKey"]
+        })
     temp = r.json()["main"]["temp"]
 
     print(temp)

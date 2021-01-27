82fbea1145f5192934d3429d359415651c890e26
Ken Lim <ken@unintendeduse.com>
2021-01-27 22:20:02 +0000

    Renamed file

--- roomon/thermometer.py
+++ roomon/thermometer.py
@@ -3,7 +3,7 @@ import mcp9600
 import time
 
 m = mcp9600.MCP9600()
-m.set_thermocouple_type('k')
+m.set_thermocouple_type('K')
 
 # Apparently the default i2c baudrate is too high you need to lower it: 
 # https://forums.pimoroni.com/t/mcp9600-breakout-pim437/13129/3

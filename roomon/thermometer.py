b75d425aad8a84fb58d3e5e3ea0c2d3a30fe5902
Ken Lim <ken@unintendeduse.com>
2021-01-27 22:27:01 +0000

    Added notes

--- roomon/thermometer.py
+++ roomon/thermometer.py
@@ -6,6 +6,10 @@ m = mcp9600.MCP9600()
 m.set_thermocouple_type('K')
 
 # Apparently the default i2c baudrate is too high you need to lower it: 
+# set the followig line in the Pi's /boot/config.txt file
+# dtparam=i2c_arm=on,i2c_arm_baudrate=40000
+
+# Source:
 # https://forums.pimoroni.com/t/mcp9600-breakout-pim437/13129/3
 # https://www.raspberrypi-spy.co.uk/2018/02/change-raspberry-pi-i2c-bus-speed/
 
